2a2f0ca190543ef1e27c45483302a6dbf1d2f184
Ken Henson Beltran <ken.beltran@bullshark.studio>
2026-05-01 00:28:23 +0800

    fixed unit test warning

--- project/apps/frontend/src/components/icons/BurgerIcon.tsx
+++ project/apps/frontend/src/components/icons/BurgerIcon.tsx
@@ -9,7 +9,7 @@ export default function BurgerIcon() {
       fill="none"
       viewBox="0 0 24 24"
     >
-      <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" />
+      <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14" />
     </svg>
   )
 }
